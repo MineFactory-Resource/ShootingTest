bd549df22a0a3c95c8fd0f18fc82e759adb532a1
Seokhun-Yang <ddayang1234@gmail.com>
2022-09-21 15:41:33 +0900

    클래스명 대소문자 변경

--- src/main/java/net/teamuni/shootingtest/events/playerTeleport.java
+++ src/main/java/net/teamuni/shootingtest/events/playerTeleport.java
@@ -4,7 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
-public class playerTeleport implements Listener {
+public class PlayerTeleport implements Listener {
 
     @EventHandler
     public void onTeleport(PlayerTeleportEvent event) {
