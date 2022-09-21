160e300d18cf087192584f9a6e89a54e1ff73b6d
Seokhun-Yang <ddayang1234@gmail.com>
2022-09-21 15:35:37 +0900

    플레이어 텔레포트를 listen하는 클래스 추가

---
+++ src/main/java/net/teamuni/shootingtest/events/playerTeleport.java
@@ -0,0 +1,13 @@
+package net.teamuni.shootingtest.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+public class playerTeleport implements Listener {
+
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent event) {
+
+    }
+}
