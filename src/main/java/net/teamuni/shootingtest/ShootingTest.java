72d122d9993bdf7e7399e0dce21466e2db67100d
Seokhun-Yang <ddayang1234@gmail.com>
2022-09-21 15:33:14 +0900

    config.yml 추가

--- src/main/java/net/teamuni/shootingtest/ShootingTest.java
+++ src/main/java/net/teamuni/shootingtest/ShootingTest.java
@@ -6,8 +6,7 @@ public final class ShootingTest extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        saveDefaultConfig();
     }
 
     @Override
