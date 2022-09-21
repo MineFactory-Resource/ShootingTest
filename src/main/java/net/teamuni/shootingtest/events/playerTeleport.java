1e637c9a989695ea3359d8b8b754705c477c8576
Seokhun-Yang <ddayang1234@gmail.com>
2022-09-21 21:23:32 +0900

    config.yml에서 사격장 월드 설정할 수 있도록 변경

--- src/main/java/net/teamuni/shootingtest/events/playerTeleport.java
+++ src/main/java/net/teamuni/shootingtest/events/playerTeleport.java
@@ -8,6 +8,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
+import java.util.List;
+
 public class PlayerTeleport implements Listener {
     private final ShootingTest main = ShootingTest.getInstance();
     private final PlayerInv inv = PlayerInv.getPlayerInv();
@@ -15,9 +17,11 @@ public class PlayerTeleport implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
     public void onTeleport(PlayerTeleportEvent event) {
         Player player = event.getPlayer();
-        if (event.getTo().getWorld().getName().equals(main.getConfig().getString("range.world"))) {
+        List<String> worlds = main.getConfig().getStringList("enable_world");
+
+        if (worlds.contains(event.getTo().getWorld().getName())) {
             inv.setPlayerInv(player);
-        } else if (event.getFrom().getWorld().getName().equals(main.getConfig().getString("range.world"))) {
+        } else if (worlds.contains(event.getFrom().getWorld().getName())) {
             inv.returnPlayerInv(player);
         }
     }
