394bf72be5b6fcd3b83408a0e63826ec3e67eaf1
Seokhun-Yang <ddayang1234@gmail.com>
2022-09-24 21:33:40 +0900

    feature/DEV-281 브랜치에서 pull 후 merge

--- src/main/java/net/teamuni/shootingtest/config/ItemManager.java
+++ src/main/java/net/teamuni/shootingtest/config/ItemManager.java
@@ -60,7 +60,7 @@ public class ItemManager {
     @NotNull
     public Map<Integer, ItemStack> getItems(String path) {
         Map<Integer, ItemStack> items = new HashMap<>();
-        Set<String> itemKeys = this.get().getConfigurationSection(path).getKeys(false);
+        Set<String> itemKeys = this.getConfig().getConfigurationSection(path).getKeys(false);
         if (itemKeys.isEmpty()) {
             throw new IllegalArgumentException("items.yml에서 정보를 가져오는 도중 문제가 발생했습니다.");
         }
