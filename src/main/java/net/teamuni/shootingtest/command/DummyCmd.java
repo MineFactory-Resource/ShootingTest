a444efd233202626ca326eb4ed80c626a80a2470
Seokhun-Yang <ddayang1234@gmail.com>
2022-09-30 00:10:24 +0900

    입력한 명령어가 양식에 맞지 않으면 명령어 실행 취소

--- src/main/java/net/teamuni/shootingtest/command/DummyCmd.java
+++ src/main/java/net/teamuni/shootingtest/command/DummyCmd.java
@@ -1,6 +1,7 @@
 package net.teamuni.shootingtest.command;
 
 import net.teamuni.shootingtest.ShootingTest;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,11 +18,16 @@ public class DummyCmd implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player player) {
             if (command.getName().equalsIgnoreCase("dummy") && player.hasPermission("st.manage")) {
+                if (args.length != 2) {
+                    String message = main.getMessageManager().getConfig().getString("incorrect_command", "&cThe command cannot be executed!");
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+                    return false;
+                }
                 if (args[0].equalsIgnoreCase("create")) {
-                    main.getDummy().createDummy(player, args[1], player.getLocation());
+                    main.getDummyManager().createDummy(player, args[1], player.getLocation());
                     return false;
                 } else if (args[0].equalsIgnoreCase("remove")) {
-                    main.getDummy().removeDummy(player, args[1]);
+                    main.getDummyManager().removeDummy(player, args[1]);
                     return false;
                 }
             }
